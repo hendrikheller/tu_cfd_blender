46369042d2b4c39fb2a8494489d70a5660836c26
hendrikheller <hendrik.heller@campus.tu-berlin.de>
2014-09-07 15:30:42 +0200

    adding comments and docstrings

--- tu_cfd_blender.py
+++ tu_cfd_blender.py
@@ -1,15 +1,28 @@
 __author__ = 'Hendrik Heller'
 
 import os
+import time
 import numpy as np
 import bpy
 
+#these paths are, of course, only for temporary testing
 path_state = "d:/blender kram/uhareksches ding/state_square.dat"
 path_foout = "d:/blender kram/uhareksches ding/foout_rect.dat"
 
 
 def clean_line(s):
-    # eliminate duplicate spaces and whitespaces from lines
+    """Cleans a string of multiple whitespaces and line breaks
+
+    Parameters
+    ----------
+    s : string
+        The string to be cleaned.
+
+    Returns
+    -------
+    t : string
+        A string void of double white spaces and line breaks.
+    """
     assert isinstance(s, str)
     t = " ".join(s.rstrip().split())
     assert only_single_spaces(t)
@@ -18,6 +31,20 @@ def clean_line(s):
 
 
 def parse_state(path):
+    """Parse a state.dat file.
+
+    long description goes here
+
+    Parameters
+    ----------
+    path : string
+        The system path to the state.dat file.
+
+    Returns
+    -------
+    t : string
+        A string void of double white spaces and line breaks.
+    """
     assert isinstance(path, str)
     # transform path for os specifics
     path = os.path.normpath(path)
@@ -42,7 +69,12 @@ def parse_state(path):
 
 
 class StateDat:
-    """class documentation"""
+    """A class to better store the results of parsing a state.dat file and make them more accessible.
+
+    Attributes:
+    -----------
+    varmapping : dictionary
+        Assignes every of the 28 parsed values an index. This is where the value is stored within the internal array."""
     varmapping = {"t": 0, "gra1": 1, "gra2": 2, "gra3": 3,
                   "uship": 4, "vship": 5, "wship": 6, "upkt": 7, "vpkt": 8, "wpkt": 9,
                   "ome1": 10, "ome2": 11, "ome3": 12, "ppkt": 13, "qpkt": 14, "rpkt": 15,
@@ -50,10 +82,39 @@ class StateDat:
                   "xor": 22, "yor": 23, "zor": 24, "ufest": 25, "vfest": 26, "wfest": 27}
 
     def __init__(self, dat):
+        """Initializes a new 'StateDat' object
+
+        Parameters
+        ----------
+        dat : [[t, gra1, gra2, gra3, uship, ...], ...]
+            Any structure an nd-array can be initialized with, containing the data in the predefined order.
+        """
         self.data = np.array(dat)
 
     def get_step(self, step):
+        """Returns the data of a specific step.
+
+        long description goes here
+
+        Parameters
+        ----------
+        step : int
+            The timestep to be returned.
+
+        Returns
+        -------
+        data[step] : [values]
+            A list of the values of data at the specified timestep.
+        """
         return self.data[step]
+
+    def get_var(self, var):
+        assert isinstance(var, str)
+        var = var.lower()
+        if var in self.varmapping:
+            return self.data[:, self.varmapping[var]]
+        else:
+            raise Exception("Var could not be matched.")
 
     def get_step_var(self, step, var):
         assert isinstance(var, str)
@@ -61,14 +122,6 @@ class StateDat:
         var = var.lower()
         if var in self.varmapping:
             return self.data[step, self.varmapping[var]]
-        else:
-            raise Exception("Var could not be matched.")
-
-    def get_var(self, var):
-        assert isinstance(var, str)
-        var = var.lower()
-        if var in self.varmapping:
-            return self.data[:, self.varmapping[var]]
         else:
             raise Exception("Var could not be matched.")
 
@@ -143,7 +196,7 @@ def calc_steps(start, stop, amount):
     return ret
 
 
-def insert_shapekey_keyframes(key, k, smoothing_amount, smoothing_function):
+def insert_shapekey_keyframes(key, k, smoothing_function, smoothing_amount):
     steps = calc_steps(-1, 1, 3+(smoothing_amount*2))
     for i, s in enumerate(steps):
         key.value = smoothing_function(s)
@@ -152,13 +205,31 @@ def insert_shapekey_keyframes(key, k, smoothing_amount, smoothing_function):
 
 def create_mesh(name, vertex_data, face_data):
     mesh = bpy.data.meshes.new("mesh")
-    object = bpy.data.objects.new(name, mesh)
-    object.location = bpy.context.scene.cursor_location
-    bpy.context.scene.objects.link(object)
+    ob = bpy.data.objects.new(name, mesh)
+    ob.location = bpy.context.scene.cursor_location
+    bpy.context.scene.objects.link(ob)
     mesh.from_pydata(vertex_data, [], face_data)
     mesh.update(calc_edges=True)
-    # object.shade_smooth()
-    return object
+    # ob.shade_smooth()
+    return ob
+
+
+def create_animated_surface(name, foout_data, smoothing_function, smoothing_amount):
+    ob = create_mesh(name, foout_data[0][0], calc_face_mapping(foout_data[1][0], foout_data[1][1]))
+
+    # Add Basis key
+    ob.shape_key_add(from_mix=False)
+
+    # Add a shape key for every step in foout
+    for k, foout_step in enumerate(da[0]):
+        key = ob.shape_key_add("key_t" + str(k), from_mix=False)
+        insert_shapekey_keyframes(key, k, smoothing_function, smoothing_amount)
+
+        for i in range(len(key.data)):
+            pt = key.data[i].co
+            pt[0] = da[0][k][i][0]
+            pt[1] = da[0][k][i][1]
+            pt[2] = da[0][k][i][2]
 
 
 def lin(x):
@@ -182,46 +253,16 @@ def only_single_spaces(s):
                 return False
         else:
             space = False
-
     return True
 
-import time
-current_milli_time = lambda: int(round(time.time() * 1000))
+
+def current_milli_time():
+    return int(round(time.time() * 1000))
 # ----------------------
-
-# if False:
-#     import matplotlib.pyplot as plt
-#     plt.subplot(111)
-#     plt.plot(filedat.get_var("uship"), label="uship")
-#     plt.plot(filedat.get_var("vship"), label="vship")
-#     plt.plot(filedat.get_var("ang3"), label="ang3")
-#     plt.plot(filedat.get_var("ang3p"), label="ang3p")
-#     plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
-#     plt.grid()
-#     plt.show()
-
-
-def create_animated_surface(name, foout_data, smoothing_func, smoothing_amount):
-    ob = create_mesh(name, foout_data[0][0], calc_face_mapping(foout_data[1][0], foout_data[1][1]))
-
-    # Add Basis key
-    ob.shape_key_add(from_mix=False)
-
-    # Add a shape key for every step in foout
-    for k, foout_step in enumerate(da[0]):
-        key = ob.shape_key_add("key_t" + str(k), from_mix=False)
-        insert_shapekey_keyframes(key, k, smoothing_amount, smoothing_func)
-
-        for i in range(len(key.data)):
-            pt = key.data[i].co
-            pt[0] = da[0][k][i][0]
-            pt[1] = da[0][k][i][1]
-            pt[2] = da[0][k][i][2]
 
 
 t0 = current_milli_time()
 da = parse_foout(path_foout)
-#print (da[1][0], da[1][1])
 
 mapping = calc_face_mapping(da[1][0], da[1][1])
 
@@ -232,24 +273,5 @@ create_animated_surface("surface_quad_2", da, quad, 2)
 
 t1 = current_milli_time()
 
-# ob = create_mesh("surface", da[0][0], mapping)
-
-
-# Add Basis key
-# ob.shape_key_add(from_mix=False)
-
-# Add a shape key for every step in foout
-# for k, foout_step in enumerate(da[0]):
-#     key = ob.shape_key_add("key_t" + str(k), from_mix=False)
-#     insert_shapekey_keyframes(key, k, 0, lin)
-#
-#     for i in range(len(key.data)):
-#         pt = key.data[i].co
-#         pt[0] = da[0][k][i][0]
-#         pt[1] = da[0][k][i][1]
-#         pt[2] = da[0][k][i][2]
-
 
 print ("runtime in millis: " + str(t1-t0))
-#print len(mapping)
-
